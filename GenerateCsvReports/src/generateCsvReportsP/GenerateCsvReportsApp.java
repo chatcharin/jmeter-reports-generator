dfd75c8174392cedd5d9ceb90a1a6b131074e133
jovarask@gmail.com <jovarask@gmail.com@90c8ac63-92d0-2316-734c-97e4ac3d65f3>
2011-09-18 17:56:31 +0000

    TEST1

---
+++ GenerateCsvReports/src/generateCsvReportsP/GenerateCsvReportsApp.java
@@ -0,0 +1,45 @@
+/*
+ * GenerateCsvReportsApp.java
+ */
+
+package generateCsvReportsP;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class GenerateCsvReportsApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new GenerateCsvReportsWorker(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+        
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of GenerateCsvReportsApp
+     */
+    public static GenerateCsvReportsApp getApplication() {
+        return Application.getInstance(GenerateCsvReportsApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(GenerateCsvReportsApp.class, args);
+    }
+}
