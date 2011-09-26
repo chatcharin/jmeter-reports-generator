954571588540bd1faaee80755d4e24d6e0993d62
jovarask@gmail.com <jovarask@gmail.com@90c8ac63-92d0-2316-734c-97e4ac3d65f3>
2011-09-26 21:31:11 +0000

    PNG's, csv's - ok TODO: trimmed

--- GenerateCsvReports/src/generateCsvReportsP/GenSettingsWorker.java
+++ GenerateCsvReports/src/generateCsvReportsP/GenSettingsWorker.java
@@ -3,9 +3,6 @@ package generateCsvReportsP;
 
 import kg.apc.jmeter.PluginsCMDWorker;
 import java.io.File;
-import java.util.Locale;
-import java.util.ArrayList;
-
 /**
  *
  * @author Jovaras * 
@@ -22,40 +19,49 @@ import java.util.ArrayList;
 //        pluginType = "HitsPerSecond";
 //        pluginType = "LatenciesOverTime";
  */
+//OUTSIDE WORLD COMMUNICATOR
 public class GenSettingsWorker {
-    public static char CSV_DELIM = ';';   
-//    public static ArrayList<Boolean> colsArr = new ArrayList<Boolean>();
+    public static Boolean [] colsArray = new Boolean [8];
+
     public static void init_PNG(File in, File out){
 
-        PluginsCMDWorker.addExportMode(1); //PNG
         PluginsCMDWorker.setInputFile(in.toString());
         PluginsCMDWorker.setOutputPNGFile(out.toString());
-        PluginsCMDWorker.setPluginType("ResponseTimesOverTime");
-        PluginsCMDWorker.initializeProperties();
         PluginsCMDWorker.doJob();
     }
     
     public static void init_CSV(File in, File out){
         
-        PluginsCMDWorker.addExportMode(2); //CSV
         PluginsCMDWorker.setInputFile(in.toString());
         PluginsCMDWorker.setOutputCSVFile(out.toString());
-        PluginsCMDWorker.setPluginType("AggregateReport");
-        PluginsCMDWorker.initializeProperties();
-//        org.apache.jmeter.save.CSVSaveService.colsArr = colsArr;
-        org.apache.jmeter.save.CSVSaveService.setDelim(CSV_DELIM);
         PluginsCMDWorker.doJob();
     }
-   public static Boolean checkForErrors(){
-       if (org.apache.jmeter.save.CSVSaveService.CONTAINS_ERRORS) return true;
-       else return false;
-   }
-    
+    public static Boolean checkForErrors(){
+        if (org.apache.jmeter.save.CSVSaveService.CONTAINS_ERRORS) return true;
+        else return false;
+    }    
     public static void setMainComms(){
         PluginsCMDWorker.setMainCommsOnly(Boolean.TRUE);
     }
     public static void setAllComms(){
         PluginsCMDWorker.setMainCommsOnly(Boolean.FALSE);
+    }
+    public static void setStepsArray(){
+        org.apache.jmeter.save.CSVSaveService.colsArr = colsArray;
+    }
+    public static void initJMeterProps(){
+        PluginsCMDWorker.initializeProperties();
+    }
+    public static void setExportMode(int mode){
+        if (mode == 1) PluginsCMDWorker.addExportMode(1);// PNG
+        else PluginsCMDWorker.addExportMode(2);//CSV
+    }
+    public static void setPluginType(int type){
+        if (type == 1) PluginsCMDWorker.setPluginType("ResponseTimesOverTime");
+        else PluginsCMDWorker.setPluginType("AggregateReport");
+    }
+    public static void setCSVDelim(char delim){
+        org.apache.jmeter.save.CSVSaveService.setDelim(delim);
     }
         
     //TODO: check if everything is set. Maybe in doJob()'s class?
